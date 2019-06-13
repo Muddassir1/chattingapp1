5b5c25e0dce31730c810cbb7371981d718e16282
Muddassir Ahmed <muddassir.ah@gmail.com>
2019-06-13 21:18:55 +0500

    added subscription and login 2

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
-//var socket = io.connect('https://chattingapp1.herokuapp.com');
-var socket = io.connect('http://localhost:5000');
+var socket = io.connect('https://chattingapp1.herokuapp.com');
+//var socket = io.connect('http://localhost:5000');
 
 socket.on('userConnected',function(data){
 	console.log(data);
